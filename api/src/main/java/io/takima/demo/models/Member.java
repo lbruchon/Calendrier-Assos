69f216a0c03f78e1af479f7bbe48aec4259e9a40
Tibserre <thibault.serre@epfedu.fr>
2022-09-22 18:00:00 +0200

    Models OK

--- api/src/main/java/io/takima/demo/models/Member.java
+++ api/src/main/java/io/takima/demo/models/Member.java
@@ -1,6 +1,7 @@
 package io.takima.demo.models;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Table(name="members")
@@ -17,4 +18,79 @@ public class Member {
     @ManyToOne
     @JoinColumn(name="associationId", nullable = false)
     private Association association;
+
+    public Member() {
+    }
+
+    public Member(Long member_id, String memberEmail, String memberMdp, Boolean memberSuperadmin, Association association) {
+        this.member_id = member_id;
+        this.memberEmail = memberEmail;
+        this.memberMdp = memberMdp;
+        this.memberSuperadmin = memberSuperadmin;
+        this.association = association;
+    }
+
+    public Long getMember_id() {
+        return member_id;
+    }
+
+    public void setMember_id(Long member_id) {
+        this.member_id = member_id;
+    }
+
+    public String getMemberEmail() {
+        return memberEmail;
+    }
+
+    public void setMemberEmail(String memberEmail) {
+        this.memberEmail = memberEmail;
+    }
+
+    public String getMemberMdp() {
+        return memberMdp;
+    }
+
+    public void setMemberMdp(String memberMdp) {
+        this.memberMdp = memberMdp;
+    }
+
+    public Boolean getMemberSuperadmin() {
+        return memberSuperadmin;
+    }
+
+    public void setMemberSuperadmin(Boolean memberSuperadmin) {
+        this.memberSuperadmin = memberSuperadmin;
+    }
+
+    public Association getAssociation() {
+        return association;
+    }
+
+    public void setAssociation(Association association) {
+        this.association = association;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Member member = (Member) o;
+        return Objects.equals(member_id, member.member_id) && Objects.equals(memberEmail, member.memberEmail) && Objects.equals(memberMdp, member.memberMdp) && Objects.equals(memberSuperadmin, member.memberSuperadmin) && Objects.equals(association, member.association);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(member_id, memberEmail, memberMdp, memberSuperadmin, association);
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "member_id=" + member_id +
+                ", memberEmail='" + memberEmail + '\'' +
+                ", memberMdp='" + memberMdp + '\'' +
+                ", memberSuperadmin=" + memberSuperadmin +
+                ", association=" + association +
+                '}';
+    }
 }
--- api/src/main/java/io/takima/demo/models/Tag.java
+++ api/src/main/java/io/takima/demo/models/Tag.java
@@ -19,5 +19,56 @@ public class Tag {
     public Tag() {
     }
 
+    public Tag(Long tag_id, String tagName, Set<Post> posts) {
+        this.tag_id = tag_id;
+        this.tagName = tagName;
+        this.posts = posts;
+    }
 
+    public Long getTag_id() {
+        return tag_id;
+    }
+
+    public void setTag_id(Long tag_id) {
+        this.tag_id = tag_id;
+    }
+
+    public String getTagName() {
+        return tagName;
+    }
+
+    public void setTagName(String tagName) {
+        this.tagName = tagName;
+    }
+
+    public Set<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(Set<Post> posts) {
+        this.posts = posts;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Tag tag = (Tag) o;
+        return Objects.equals(tag_id, tag.tag_id) && Objects.equals(tagName, tag.tagName) && Objects.equals(posts, tag.posts);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(tag_id, tagName, posts);
+    }
+
+    @Override
+    public String toString() {
+        return "Tag{" +
+                "tag_id=" + tag_id +
+                ", tagName='" + tagName + '\'' +
+                ", posts=" + posts +
+                '}';
+    }
 }
