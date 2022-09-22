5dec144e3601e99f9ab941bccaf3804a37c9ccaf
Tibserre <thibault.serre@epfedu.fr>
2022-09-22 18:14:02 +0200

    DAO done

---
+++ api/src/main/java/io/takima/demo/DAO/MemberDAo.java
@@ -0,0 +1,9 @@
+package io.takima.demo.DAO;
+
+import io.takima.demo.models.Member;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberDAo extends CrudRepository<Member, Long> {
+}
