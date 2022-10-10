ad159bb7e852c98edf4487aae735f85f805ec759
lbruchon <lbruchon@takima.fr>
2022-10-10 09:55:38 +0200

    setup front connexion page

---
+++ front/src/app/connexion/connexion.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import {NgForm} from "@angular/forms";
+import {User} from "../../models/user.model";
+import {HttpClient} from "@angular/common/http";
+import {Router} from "@angular/router";
+import {UserService} from "../services/user.service";
+
+@Component({
+  selector: 'app-connexion',
+  templateUrl: './connexion.component.html',
+  styleUrls: ['./connexion.component.scss']
+})
+
+export class ConnexionComponent implements OnInit {
+
+  constructor(private httpClient: HttpClient, private router: Router, private userService: UserService) {
+  }
+
+  ngOnInit() {
+  }
+
+  onSubmit(ngForm: NgForm) {
+    console.log(ngForm);
+    const user = new User(
+      null,
+      ngForm.form.value.firstName,
+      ngForm.form.value.lastName,
+      ngForm.form.value.age,
+    )
+
+    this.userService.addUser(user).subscribe();
+    setTimeout(()=>this.router.navigateByUrl('/'), 1000)
+  }
+
+
+}
