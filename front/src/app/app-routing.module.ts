0f3c507cb13fa4037d98d0a835740d337190106b
Tibserre <thibault.serre@epfedu.fr>
2022-10-10 10:16:16 +0200

    Page accueil créee

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import {RouterModule, Routes} from '@angular/router';
 import {AddUserComponent} from "./add-user/add-user.component";
 import {ListUsersComponent} from "./list-users/list-users.component";
 import {ConnexionComponent} from "./connexion/connexion.component";
+import { AccueilComponent } from './accueil/accueil.component';
 
 const routes: Routes = [
   {path:'', component: ListUsersComponent},
   {path:'add-user', component: AddUserComponent},
   {path:'connexion', component: ConnexionComponent},
+  {path:'index', component: AccueilComponent},
 ];
 
 
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AddUserComponent } from './add-user/add-user.component';
 import { ListUsersComponent } from './list-users/list-users.component';
 import {FormsModule} from "@angular/forms";
 import {ConnexionComponent} from "./connexion/connexion.component";
+import { AccueilComponent } from './accueil/accueil.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AddUserComponent,
     ListUsersComponent,
-    ConnexionComponent
+    ConnexionComponent,
+    AccueilComponent
   ],
   imports: [
     BrowserModule,
